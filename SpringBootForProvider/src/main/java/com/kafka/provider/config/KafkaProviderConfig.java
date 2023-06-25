b06aed58b50ce7a3cc2a5fbf5840aa576c535c03
Graciela Contreras <gracielaestefani@gmail.com>
2023-06-24 23:15:26 -0400

    Creando mensajes para que se envíen al servidor apache Kafka (Provider o Producer)

---
+++ SpringBootForProvider/src/main/java/com/kafka/provider/config/KafkaProviderConfig.java
@@ -0,0 +1,41 @@
+package com.kafka.provider.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProviderConfig {
+
+    @Value("${spring.kafka.bootstrapServers}")
+    private String bootstrapServers;
+
+//    configuramos el proveedor de mensajes de kafka
+    public Map<String, Object> producerConfig(){
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); //se va a enviar el mensaje de tipo String
+        return properties;
+    }
+    //    se configura el cliente
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+//    se crea el objeto que va a encargarse de enviar el mensaje
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
---
+++ SpringBootForProvider/src/main/java/com/kafka/provider/models/MessageRequest.java
@@ -0,0 +1,4 @@
+package com.kafka.provider.models;
+
+public record MessageRequest(String message) {
+}
